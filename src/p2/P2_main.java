fd106e38043d3954c9657ce4e0f15f3e092030ec
Chris24305 <christian.schaa@web.de>
2025-10-30 10:43:15 +0100

    P3 erstellt

--- src/p2/P2_main.java
+++ src/p2/P2_main.java
@@ -1,8 +1,16 @@
 package p2;
 
 public class P2_main {
-	public static void main(String[] args){
-	System.out.println("Hello World!");
+	public static void main(String[] args) {
+		int i;
+		boolean b;
+		i = 11;
+		b = false;
+		if (i > 10) {
+			b = true;
+		}
+		System.out.println(b);
+		System.out.print(i);
 	}
 
 }
